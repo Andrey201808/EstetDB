a623665619182a6277b624c0f2c01bd8d82dc961
Andrey201808 <44214065+Andrey201808@users.noreply.github.com>
2023-06-09 02:11:33 +0300

    Add files via upload
    Обработка входящих сообщений сервера

---
+++ Incoming.py
@@ -0,0 +1,18 @@
+async def app(scope, receive, send):
+    """
+    Отображение метода и передача обратно в ответ HTTP.
+    """
+    assert scope['type'] == 'http'
+
+    body = f'Received {scope["method"]} request to {scope["path"]}'
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ]
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': body.encode('utf-8'),
+    })
