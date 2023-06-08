60a24602604a5638003f27e6e1ef3eca3087a66f
Andrey201808 <44214065+Andrey201808@users.noreply.github.com>
2023-06-09 02:14:25 +0300

    Add files via upload
    Отправка медленного HTTP ответа клиенту.

---
+++ streaming.py
@@ -0,0 +1,25 @@
+import asyncio
+
+
+async def app(scope, receive, send):
+    """
+    Отправка медленного HTTP ответа клиенту.
+    """
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ]
+    })
+    for chunk in [b'Connect', b', ', 'Server']:
+        await send({
+            'type': 'http.response.body',
+            'body': chunk,
+            'more_body': True
+        })
+        await asyncio.sleep(1)
+    await send({
+        'type': 'http.response.body',
+        'body': b'',
+    })
