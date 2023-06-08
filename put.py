790861d91d3355d584b5497012b7adb858d68e88
Andrey201808 <44214065+Andrey201808@users.noreply.github.com>
2023-06-09 01:39:03 +0300

    Add files via upload
    Метод Put для сервера

---
+++ put.py
@@ -0,0 +1,9 @@
+import requests
+url = 'https://superestet.ru/'
+payload  = {'method': 'getQuote', 'format': 'json', 'lang': 'ru'}
+res = requests.put(url, params=payload)
+print('url ',res.url)
+print('Код состояния ',res.status_code)
+print('Заголовки ответа ',res.headers)
+print('Содержимое ответа ',res.text)
+print('Содержимое ответа ',res.content)
